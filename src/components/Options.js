ecbe46c93e54becead31b32716c4cbd2b4201963
Harrison <52066364+HarrisonKN@users.noreply.github.com>
2025-02-16 11:41:10 +1100

    Changed Par name

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,7 +4,7 @@ import '../styles/Options.css';
 const Options = ({ onPar99 }) => {
     return (
         <div className="options">
-            <button onClick={onPar99}>Par 99</button>
+            <button onClick={onPar99}>Beginner Par</button>
             <p>Adds +1 to HCP Holes 10-18, and Adds +2 to HCP Holes 1-9</p>
         </div>
     );
